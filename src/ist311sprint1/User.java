f086cc367a6bb268e6ff1dea00c69a9e43553c06
bso5036 <bso5036@E165-WL45.up.ist.local>
2018-10-28 16:01:15 -0400

    initial commit

---
+++ src/ist311sprint1/User.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ist311sprint1;
+
+/**
+ *
+ * @author bso5036
+ */
+public class User {
+    private String username;
+    private String password;
+    
+    public User(String inUsername, String inPassword){
+        username = inUsername;
+        password = inPassword;
+    }
+    
+    @Override
+    public String toString(){
+        return "Username: " + username + " Password: " + password;
+    }
+}
---
+++ src/ist311sprint1/UserList.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ist311sprint1;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author bso5036
+ */
+public class UserList {
+    ArrayList<User> userList;
+    public UserList(){
+        userList = new ArrayList<>();
+        addUsersToList();
+        System.out.println(userList.get(0).toString());
+    }
+    
+    private void addUsersToList(){
+        userList.add(new User("12345678", "12345678"));
+        userList.add(new User("password", "password"));
+        userList.add(new User("rockyou", "rockyou"));
+        userList.add(new User("babygirl", "babygirl"));
+        userList.add(new User("iloveyou", "iloveyou"));
+    }
+}
