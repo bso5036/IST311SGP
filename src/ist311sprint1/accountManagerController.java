99b52fcd152dbb884cc88c5866fa12829ed21f8e
bso5036 <bso5036@E165-WL45.up.ist.local>
2018-10-28 17:41:10 -0400

    Changed accountManagerController package to ist311sprint1

--- src/ist311sprint1/accountManagerController.java
+++ src/ist311sprint1/accountManagerController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package m08.a01;
+package ist311sprint1;
 
 import java.net.URL;
 import java.util.ResourceBundle;
