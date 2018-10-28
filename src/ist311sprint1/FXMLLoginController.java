eedc55c7ccc8163ccb0648aecd4c633a76f28b62
bso5036 <bso5036@E165-WL45.up.ist.local>
2018-10-28 18:03:50 -0400

    Transitions once login is pressed

--- src/ist311sprint1/FXMLLoginController.java
+++ src/ist311sprint1/FXMLLoginController.java
@@ -5,47 +5,58 @@
  */
 package ist311sprint1;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
 /**
  *
  * @author bso5036
  */
 public class FXMLLoginController implements Initializable {
+
     private UserList userList;
-    @FXML private Text actiontarget;
-    @FXML private TextField usernameTextField;
-    @FXML private PasswordField passwordField;
-    
-    @FXML 
-    protected void handleSubmitButtonAction(ActionEvent event) {
+    @FXML
+    private Text actiontarget;
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    protected void handleSubmitButtonAction(ActionEvent event) throws IOException {
         Boolean usernameMatch = false;
         Boolean passwordMatch = false;
-        for(User user : userList.getUserList()){
-            if(user.getUsername().equals(usernameTextField.getText())){
+        for (User user : userList.getUserList()) {
+            if (user.getUsername().equals(usernameTextField.getText())) {
                 usernameMatch = true;
-                if(user.getPassword().equals(passwordField.getText())){
+                if (user.getPassword().equals(passwordField.getText())) {
                     passwordMatch = true;
                 }
             }
         }
-        if(usernameMatch && passwordMatch){
+        if (usernameMatch && passwordMatch) {
             actiontarget.setText("Logged In");
+            actiontarget.getScene().setRoot(FXMLLoader.load(getClass().getResource("accountManager.fxml")));
         } else {
             actiontarget.setText("Wrong Username/Password");
         }
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-         userList = new UserList();
-    }    
-    
+        userList = new UserList();
+    }
+
 }
