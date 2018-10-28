04d9273b997dc4e444336d72f05387d908bc8cca
bso5036 <bso5036@E165-WL45.up.ist.local>
2018-10-28 17:03:34 -0400

    Log in checks against username/password list when button is pressed

--- src/ist311sprint1/FXMLDocumentController.java
+++ src/ist311sprint1/FXMLDocumentController.java
@@ -10,26 +10,42 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 /**
  *
  * @author bso5036
  */
 public class FXMLDocumentController implements Initializable {
+    private UserList userList;
+    @FXML private Text actiontarget;
+    @FXML private TextField usernameTextField;
+    @FXML private PasswordField passwordField;
     
-    @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    @FXML 
+    protected void handleSubmitButtonAction(ActionEvent event) {
+        Boolean usernameMatch = false;
+        Boolean passwordMatch = false;
+        for(User user : userList.getUserList()){
+            if(user.getUsername().equals(usernameTextField.getText())){
+                usernameMatch = true;
+                if(user.getPassword().equals(passwordField.getText())){
+                    passwordMatch = true;
+                }
+            }
+        }
+        if(usernameMatch && passwordMatch){
+            actiontarget.setText("Logged In");
+        } else {
+            actiontarget.setText("Wrong Username/Password");
+        }
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+         userList = new UserList();
     }    
     
 }
