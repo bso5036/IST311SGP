1387bf104f99c0af45ec3d6ebc063c6758a39f23
bryanbyerly9 <44554859+bryanbyerly9@users.noreply.github.com>
2018-10-28 17:27:42 -0400

    Add files via upload

---
+++ accountManagerController.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m08.a01;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+
+/**
+ *
+ * @author bdb5328
+ */
+public class accountManagerController implements Initializable {
+    
+    //@FXML
+    //private Label label;
+    @FXML
+    private Text outputText;
+
+    /*
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        
+        label.setText("Hello World!");
+    }
+*/
+    @FXML
+    public void handleLogOutButtonAction(ActionEvent event){
+        outputText.setText("Logout button pressed");
+    }
+    public void handleViewScheduleButtonAction(ActionEvent event){
+        outputText.setText("View Schedule button pressed");
+    }
+    public void handleManageAccountButtonAction(ActionEvent event){
+        outputText.setText("Manage Account button pressed");
+    }
+    public void handleViewGradesButtonAction(ActionEvent event){
+        outputText.setText("View Grades button pressed");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
