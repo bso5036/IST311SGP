cffc73fd400a60f92796a578a483f9d62197d2c6
bso5036 <bso5036@E165-WL45.up.ist.local>
2018-10-28 17:34:13 -0400

    added accountManager,and hopefully fixed things because Netbeans is being mean.

--- src/ist311sprint1/accountManagerController.java
+++ src/ist311sprint1/accountManagerController.java
@@ -10,7 +10,6 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 import javafx.scene.text.Text;
 
 /**
