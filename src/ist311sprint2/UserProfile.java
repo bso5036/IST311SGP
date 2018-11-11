1135529d4472291aa5ccac5d04bde4bb9e64dc3a
bso5036 <bso5036@E165-WL45.up.ist.local>
2018-11-11 18:42:33 -0500

    Removed fxml controller code

--- src/ist311sprint2/UserProfile.java
+++ src/ist311sprint2/UserProfile.java
@@ -1,10 +1,6 @@
 package ist311sprint2;
 
 import java.util.ArrayList;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 
 /**
  *
@@ -17,16 +13,6 @@ public class UserProfile {
     private String email = new String();
     private String accessID = new String();
     private ArrayList<Course> courses;
-    @FXML
-    private TextField firstNameTextField;
-    @FXML
-    private TextField lastNameTextField;
-    @FXML
-    private TextField emailTextField;
-    @FXML
-    private TextField accessIDTextField;
-    @FXML
-    private Button submitButton;
     
 
     public UserProfile(String newFirstName, String newLastName, String newEmail, String newAccessID) {
@@ -101,16 +87,6 @@ public class UserProfile {
      */
     public void setAccessID(String accessID) {
         this.accessID = accessID;
-    }
-    
-    @FXML
-    private void handleSubmitButton(ActionEvent event) {
-        
-        setFirstName(firstNameTextField.getText());
-        setLastName(lastNameTextField.getText());
-        setEmail(emailTextField.getText());
-        setAccessID(accessIDTextField.getText());
-        
     }
     
 }
